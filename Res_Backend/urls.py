b69548c644d0b322c6618c276c2cb0a1a6c7c791
vvsatyanarayana <veerasatyanarayana111@gmail.com>
2021-02-14 23:01:52 +0530

    issue fixed

--- Res_Backend/urls.py
+++ Res_Backend/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include,re_path
 from django.conf.urls.static import static
+from . import views
 from django.views.generic import TemplateView
 from django.conf import settings
 from rest_framework.documentation import include_docs_urls
